cd31d62f84b686bee15831ec0dae689cdebaa7e0
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-03-17 15:22:45 -0400

    Word lists

---
+++ R/word_list.R
@@ -0,0 +1,9 @@
+# Generate a list of unique lowercase words used in a text
+word_list <- function(text) {
+  text %>%
+    str_split(boundary("word")) %>%
+    `[[`(1) %>%
+    str_to_lower() %>%
+    unique() %>%
+    sort()
+}
