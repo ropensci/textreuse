15ce73069c83ca955641cc387c4207efa555885f
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-03-17 14:52:02 -0400

    Add English word list

---
+++ R/data-doc.R
@@ -0,0 +1,54 @@
+#' A list of English words
+#'
+#' This list of English words includes inflected forms and British, American,
+#' and Canadian variant spellings.
+#'
+#' @format A character vector of length 348,844.
+#' @keywords words wordlist English
+#' @usage data(words_en)
+#' @source
+#' This custom wordlist was generated from \href{http://app.aspell.net/create}{SCOWL}  with these parameters:
+#' \itemize{
+#' \item diacritic: both
+#' \item max_size: 80
+#' \item max_variant: 3
+#' \item special: hacker roman-numerals
+#' \item spelling: US GBs GBz CA
+#' }
+#'
+#' The following copyright notice is required:
+#'
+#' Copyright 2000-2014 by Kevin Atkinson
+#'
+#'
+#' Permission to use, copy, modify, distribute and sell these word
+#' lists, the associated scripts, the output created from the scripts,
+#' and its documentation for any purpose is hereby granted without fee,
+#' provided that the above copyright notice appears in all copies and
+#' that both that copyright notice and this permission notice appear in
+#' supporting documentation. Kevin Atkinson makes no representations
+#' about the suitability of this array for any purpose. It is provided
+#' "as is" without express or implied warranty.
+#'
+#' Copyright (c) J Ross Beresford 1993-1999. All Rights Reserved.
+#'
+#' The following restriction is placed on the use of this publication:
+  #' if The UK Advanced Cryptics Dictionary is used in a software package
+#' or redistributed in any form, the copyright notice must be
+#' prominently displayed and the text of this document must be included
+#' verbatim.
+#'
+#' There are no other restrictions: I would like to see the list
+#' distributed as widely as possible.
+#'
+#' Special credit also goes to Alan Beale <biljir@@pobox.com> as he has
+#' given me an incredible amount of feedback and created a number of
+#' special lists (those found in the Supplement) in order to help improve
+#' the overall quality of SCOWL.
+#'
+#' Many sources were used in the creation of SCOWL, most of them were in
+#' the public domain or used indirectly.  For a full list please see the
+#' SCOWL readme.
+#'
+#' \url{http://wordlist.aspell.net/}
+"words_en"
