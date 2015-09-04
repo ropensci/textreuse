4d17d76dabf62e016bcbd9c09ba0f84c6d189766
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-09-04 16:29:06 -0400

    Jaccard dissimilarity
    Fixes #18

--- R/jaccard.R
+++ R/jaccard.R
@@ -1,10 +1,12 @@
-#' Calculate Jaccard similarity coefficient
+#' Calculate Jaccard similarity/dissimilarity coefficients
 #'
-#' This function computes a measure of similarity for two sets. The higher the
-#' number, the more similar the two sets are. When applied to two documents of
-#' class \code{\link{TextReuseTextDocument}}, the n-grams in those documents are
-#' compared. But this function can be passed sets of any class accepted by the
-#' set functions in base R. So it is possible, for instance, to pass this
+#' These functions compute the Jaccard measures of similarity or dissimilarity
+#' for two sets. The coefficients will be numbers between \code{0} and \code{1}.
+#' For the similarity coefficient, the higher the number the more similar the
+#' two sets are. When applied to two documents of class
+#' \code{\link{TextReuseTextDocument}}, the n-grams in those documents are
+#' compared. But this function can be passed objects of any class accepted by
+#' the set functions in base R. So it is possible, for instance, to pass this
 #' function two character vectors comprised of word, line, sentence, or
 #' paragraph tokens, or those character vectors hashed as integers.
 #'
@@ -13,11 +15,16 @@
 #' \deqn{J(A, B) = \frac{ | A \cup B | }{ | A \cap B | }}{ length(intersect(a,
 #' b)) / length(union(a, b))}
 #'
+#' The dissimilarity is simply
+#'
+#' \deqn{1 - J(A, B)}
+#'
 #' @param a The first set to be compared.
 #' @param b The second set to be compared.
 #'
 #' @examples
 #' jaccard_similarity(1:3, 2:4)
+#' jaccard_dissimilarity(1:3, 2:4)
 #'
 #' ny         <- system.file("extdata/ny1850-match.txt", package = "textreuse")
 #' ca_match   <- system.file("extdata/ca1851-match.txt", package = "textreuse")
@@ -33,6 +40,10 @@
 #' # These two should have a lower Jaccard similarity coefficient
 #' jaccard_similarity(ny, ca_nomatch)
 #'
+#' @name jaccard
+NULL
+
+#' @rdname jaccard
 #' @export
 jaccard_similarity <- function(a, b) UseMethod("jaccard_similarity")
 
@@ -46,4 +57,13 @@ jaccard_similarity.default <- function(a, b) {
 jaccard_similarity.TextReuseTextDocument <- function(a, b) {
   assert_that(all(class(a) == class(b)))
   jaccard_similarity(a$ngrams, b$ngrams)
+}
+
+#' @rdname jaccard
+#' @export
+jaccard_dissimilarity <- function(a, b) UseMethod("jaccard_dissimilarity")
+
+#' @export
+jaccard_dissimilarity.default <- function(a, b) {
+  1 - jaccard_similarity(a, b)
 }
--- tests/testthat/test-jaccard.R
+++ tests/testthat/test-jaccard.R
@@ -1,6 +1,6 @@
 context("Jaccard coefficients")
 
-test_that("calculates the value correctly", {
+test_that("calculates the similarity coefficient correctly", {
   expect_equal(jaccard_similarity(1:3, 2:4), 0.5)
   expect_equal(jaccard_similarity(letters[1:3], letters[2:3]), 2/3)
   a <- sample(letters, 10, replace = TRUE)
@@ -8,6 +8,13 @@ test_that("calculates the value correctly", {
   res <- jaccard_similarity(a, b)
   expect_true(0 <= res & res <= 1)
   })
+
+test_that("calculates the dissimilarity coefficient correctly", {
+  expect_equal(1 - jaccard_similarity(1:10, 8:11),
+               jaccard_dissimilarity(1:10, 8:11))
+  expect_equal(1 - jaccard_similarity(letters[1:5], letters[3:10]),
+               jaccard_dissimilarity(letters[1:5], letters[3:10]))
+})
 
 test_that("works with TextReuseTextDocument", {
   ny <- system.file("extdata/ny1850-match.txt", package = "textreuse")
