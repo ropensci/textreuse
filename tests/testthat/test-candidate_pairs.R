e84e0ee2ddc33960897ceea64075fc43fa88a3a9
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-09-11 15:48:06 -0400

    Extract candidate pairs

---
+++ tests/testthat/test-candidate_pairs.R
@@ -0,0 +1,20 @@
+context("Candidate pairs")
+dir <- system.file("extdata", package = "textreuse")
+corpus <- TextReuseCorpus(dir = dir)
+names(corpus) <- filenames(names(corpus))
+m <- pairwise_cf(corpus, ratio_of_matches, directional = TRUE)
+pairs <- candidate_pairs(m)
+
+test_that("returns a data frame with correct properties", {
+  expect_equal(nrow(pairs), 6)
+  expect_is(pairs, "data.frame")
+  expect_is(pairs, "tbl_df")
+  expect_named(pairs, c("a", "b", "score"))
+})
+
+test_that("a and b are in correct order, so results are correct", {
+  r <- sample(1:nrow(pairs), 1)
+  expect_equal(pairs$score[r],
+               ratio_of_matches(corpus[[pairs$a[r]]], corpus[[pairs$b[r]]]))
+})
+
