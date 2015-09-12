f4bb3fb75eda5ca2466ca7310d287f4ca990afac
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-09-12 14:20:14 -0400

    Rewrite minhash as function generator

--- R/find_candidates.R
+++
@@ -1,8 +0,0 @@
-
-identical(msmall[1, , drop = FALSE], msmall[3, , drop = FALSE])
-
-microbenchmark(apply(m, 1, function(x) {
-  apply(m, 1, function(y) {
-    identical(x, y)
-  })
-}), times = 5)
--- tests/testthat/test-minhash.R
+++ tests/testthat/test-minhash.R
@@ -1,16 +1,23 @@
 context("Minhash")
 
+mhash <- minhash_generator()
+file <- system.file("extdata/ny1850-match.txt", package = "textreuse")
+doc <- TextReuseTextDocument(file = file, hash_func = mhash)
+
 test_that("random integers can be generated", {
   expect_is(random_ints(3), "integer")
   expect_equal(length(random_ints(5)), 5)
 })
 
-test_that("minhash works on a character vector", {
-  random <- random_ints(5)
-  a <- minhash(c("To", "be", "or", "not", "to", "be"), random)
-  b <- minhash(c("To", "be", "whether", "or", "not"), random)
-  c <- minhash(c("This", "sentence", "is", "unlike", "the", "other"), random)
-  expect_is(a, "integer")
-  expect_equal(length(a), length(b))
-  expect_true(jaccard_similarity(a, b) > jaccard_similarity(a, c))
+test_that("minhash generator returns a proper function", {
+  expect_is(mhash, "function")
+  e <- environment(mhash)
+  expect_is(e$r, "integer")
+  expect_equal(length(e$r), 200)
+})
+
+test_that("minhash works on a TextReuseTextDocument", {
+  expect_equal(length(hashes(doc)), 200)
+  expect_is(hashes(doc), "integer")
+  expect_equal(mhash(tokens(doc)), hashes(doc))
 })
