47300bf3b5f67b6d5e29189a0173569d38c5bd62
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-10-17 14:45:51 -0400

    Tokenize and rehash write names to meta
    Fixes #38

--- R/rehash.R
+++ R/rehash.R
@@ -11,8 +11,19 @@
 #'   \code{\link{TextReuseCorpus}}.
 #' @param hash_func A function to hash the tokens. See
 #'   \code{\link{hash_string}}, \code{\link{minhash_generator}}.
+#'
 #' @return The modified \code{\link{TextReuseTextDocument}} or
 #'   \code{\link{TextReuseCorpus}}.
+#'
+#' @examples
+#' dir <- system.file("extdata/legal", package = "textreuse")
+#' minhash1 <- minhash_generator(seed = 1)
+#' corpus <- TextReuseCorpus(dir = dir, hash_func = minhash1, keep_tokens = TRUE)
+#' head(hashes(corpus[[1]]))
+#' minhash2 <- minhash_generator(seed = 2)
+#' corpus <- rehash(corpus, minhash2)
+#' head(hashes(corpus[[2]]))
+#'
 #' @export
 rehash <- function(x, hash_func) {
   UseMethod("rehash", x)
@@ -23,11 +34,13 @@ rehash.TextReuseTextDocument <- function(x, hash_func) {
   assert_that(has_tokens(x),
               is.function(hash_func))
   x$hashes <- hash_func(x$tokens)
+  x$meta$hash_func <- as.character(substitute(hash_func))
   x
 }
 
 #' @export
 rehash.TextReuseCorpus <- function(x, hash_func) {
   x$documents <- lapply(x$documents, rehash, hash_func)
+  x$meta$hash_func <- as.character(substitute(hash_func))
   x
 }
--- R/tokenize.R
+++ R/tokenize.R
@@ -15,8 +15,15 @@
 #'   returned or discarded?
 #' @param keep_text Should the text be saved in the document that is returned or
 #'   discarded?
+#'
 #' @return The modified \code{\link{TextReuseTextDocument}} or
 #'   \code{\link{TextReuseCorpus}}.
+#'
+#' @examples
+#' dir <- system.file("extdata/legal", package = "textreuse")
+#' corpus <- TextReuseCorpus(dir = dir, tokenizer = NULL)
+#' corpus <- tokenize(corpus, tokenize_ngrams)
+#' head(tokens(corpus[[1]]))
 #' @export
 tokenize <- function(x, tokenizer, ..., hash_func = hash_string,
                      keep_tokens = FALSE, keep_text = TRUE) {
@@ -35,6 +42,8 @@ tokenize.TextReuseTextDocument <- function(x, tokenizer, ...,
   x$hashes <- hash_func(x$tokens)
   if (!keep_tokens) x$tokens <- NULL
   if (!keep_text) x$text <- NULL
+  x$meta$tokenizer <- as.character(substitute(tokenizer))
+  x$meta$hash_func <- as.character(substitute(hash_func))
   x
 }
 
@@ -44,5 +53,10 @@ tokenize.TextReuseCorpus <- function(x, tokenizer, ..., hash_func = hash_string,
   x$documents <- lapply(x$documents, tokenize, tokenizer, ...,
                         hash_func = hash_func, keep_tokens = keep_tokens,
                         keep_text = keep_text)
+
+  x$meta$tokenizer <- as.character(substitute(tokenizer))
+  x$meta$hash_func <- as.character(substitute(hash_func))
+
   x
+
 }
